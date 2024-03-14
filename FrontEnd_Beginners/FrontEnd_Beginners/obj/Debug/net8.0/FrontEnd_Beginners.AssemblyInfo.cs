4bef6fd0f980258f1445336f9ef3cf70548f840f
DevAiKaiSys <dev.aikaisys@gmail.com>
2024-03-14 16:37:28 +0700

    Handling UI Events in Blazor Web Apps [Pt 6]

--- FrontEnd_Beginners/FrontEnd_Beginners/obj/Debug/net8.0/FrontEnd_Beginners.AssemblyInfo.cs
+++ FrontEnd_Beginners/FrontEnd_Beginners/obj/Debug/net8.0/FrontEnd_Beginners.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd_Beginners")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920a1fa756368fadac9b31a22726be046ce247bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+129ed38cefd773e9d609689e2bebd1bb976f4224")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd_Beginners")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontEnd_Beginners")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
